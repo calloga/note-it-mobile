177ae024bf82f9f8783b039cbb13d4307f431689
Calloga <papeabdoucalloga98@gmail.com>
2022-09-15 23:19:03 +0000

    Started Playing With React Native Following A tutorial by The Net Ninja Link : https://www.youtube.com/playlist?list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("calloga");
+  const [newName, setNewName] = useState("");
+
+  const pressHandler = () => {
+    setName(newName);
+    setNewName("");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello {name}</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Enter Name</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="e.g John Doe"
+          value={newName}
+          onChangeText={(value) => setNewName(value)}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Change Name" onPress={pressHandler} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    padding: 20,
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+  input: {
+    backgroundColor: "whitesmoke",
+    width: 200,
+    height: 40,
+    padding: 10,
+    fontSize: 20,
   },
 });
