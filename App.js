ec561ee2c7b8140abd7586c1d4bdf92b92c6a632
Calloga <papeabdoucalloga98@gmail.com>
2022-09-16 10:56:03 +0000

    Made Changes to App.js

--- App.js
+++ App.js
@@ -1,31 +1,58 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
   const [name, setName] = useState("calloga");
-  const [newName, setNewName] = useState("");
+  const [notes, setNotes] = useState([
+    {
+      key: "1",
+      title: "Note1",
+      content:
+        "Do laborum magna nostrud deserunt cillum mollit. Sunt eiusmod nostrud adipisicing nulla adipisicing veniam dolore aute consectetur aliquip commodo est enim aliqua. Reprehenderit aliqua id consequat proident Lorem Lorem dolor proident anim sit ullamco sit. Sint veniam dolore reprehenderit ipsum amet eiusmod minim et laborum aute adipisicing. Aliqua mollit velit ut ipsum enim et aute sunt laboris. Ex minim pariatur labore fugiat magna cillum. Sit amet occaecat laboris nisi eu consequat.",
+    },
+    {
+      key: "2",
+      title: "Note2",
+      content:
+        "Do laborum magna nostrud deserunt cillum mollit. Sunt eiusmod nostrud adipisicing nulla adipisicing veniam dolore aute consectetur aliquip commodo est enim aliqua. Reprehenderit aliqua id consequat proident Lorem Lorem dolor proident anim sit ullamco sit. Sint veniam dolore reprehenderit ipsum amet eiusmod minim et laborum aute adipisicing. Aliqua mollit velit ut ipsum enim et aute sunt laboris. Ex minim pariatur labore fugiat magna cillum. Sit amet occaecat laboris nisi eu consequat.",
+    },
+    {
+      key: "3",
+      title: "Note3",
+      content:
+        "Do laborum magna nostrud deserunt cillum mollit. Sunt eiusmod nostrud adipisicing nulla adipisicing veniam dolore aute consectetur aliquip commodo est enim aliqua. Reprehenderit aliqua id consequat proident Lorem Lorem dolor proident anim sit ullamco sit. Sint veniam dolore reprehenderit ipsum amet eiusmod minim et laborum aute adipisicing. Aliqua mollit velit ut ipsum enim et aute sunt laboris. Ex minim pariatur labore fugiat magna cillum. Sit amet occaecat laboris nisi eu consequat.",
+    },
+    {
+      key: "4",
+      title: "Note4",
+      content:
+        "Do laborum magna nostrud deserunt cillum mollit. Sunt eiusmod nostrud adipisicing nulla adipisicing veniam dolore aute consectetur aliquip commodo est enim aliqua. Reprehenderit aliqua id consequat proident Lorem Lorem dolor proident anim sit ullamco sit. Sint veniam dolore reprehenderit ipsum amet eiusmod minim et laborum aute adipisicing. Aliqua mollit velit ut ipsum enim et aute sunt laboris. Ex minim pariatur labore fugiat magna cillum. Sit amet occaecat laboris nisi eu consequat.",
+    },
+  ]);
 
-  const pressHandler = () => {
-    setName(newName);
-    setNewName("");
-  };
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.boldText}>Hello {name}</Text>
       </View>
       <View style={styles.body}>
-        <Text>Enter Name</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="e.g John Doe"
-          value={newName}
-          onChangeText={(value) => setNewName(value)}
+        <FlatList
+          data={notes}
+          renderItem={({ item }) => (
+            <View style={styles.note}>
+              <Text>{item.title}</Text>
+            </View>
+          )}
         />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button title="Change Name" onPress={pressHandler} />
       </View>
       <StatusBar style="auto" />
     </View>
@@ -49,14 +76,13 @@ const styles = StyleSheet.create({
   body: {
     padding: 20,
   },
-  buttonContainer: {
-    marginTop: 20,
-  },
-  input: {
-    backgroundColor: "whitesmoke",
-    width: 200,
-    height: 40,
+  note: {
+    borderColor: "pink",
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    width: 360,
+    height: 100,
     padding: 10,
-    fontSize: 20,
   },
 });
